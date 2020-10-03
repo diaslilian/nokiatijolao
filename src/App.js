e3bbb30df4e739ff039fdc0688b2b5e5d14b7a83
diaslilian <lilian.dias25@gmail.com>
2020-10-03 18:48:19 -0300

    import project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
-import './app.scss'
+import "./app.scss";
 
 function App() {
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      new Promise((r) => setTimeout(r, 500));
+      alert(JSON.stringify(e, null, 2));
+    }
+  };
+
   return (
     <div className="App">
       <div className="container">
-        
+        <div className="box-text">
+          <p>Accenture</p>
+
+          <fieldset>
+            <input type="text" onKeyDown={handleKeyDown} />
+          </fieldset>
+        </div>
       </div>
     </div>
   );
